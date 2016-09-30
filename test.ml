0c72d8f49eef702c4de8c2d51f2d98af4a08e5eb
Dillon J Fearns <djf1@hw.ac.uk>
2016-09-30 04:18:12 +0100

    Added boolean test functions

--- test.ml
+++ test.ml
@@ -2,21 +2,29 @@ fun testFunction l n f r =
   if f = r then ("Question " ^ l ^ ", number " ^ n ^ ": PASS")
   else ("Question " ^ l ^ ", number " ^ n ^ ": FAIL");
 
+fun assertsTrue f q n =
+  if (f = true) then ("Question " ^ q ^ ", number " ^ n ^ ": PASS")
+  else ("Question " ^ q ^ ", number " ^ n ^ ": FAIL");
+
+fun assertsFalse f q n =
+  if (f = false) then ("Question " ^ q ^ ", number " ^ n ^ ": PASS")
+  else ("Question " ^ q ^ ", number " ^ n ^ ": FAIL");
+
 use "answers.ml";
 
 (* A 1 *)
 realIntMult 3.3 3;
 (* A 2 *)
-moreLetters "big" "small";
+assertsFalse (moreLetters "big" "small") "A" "2";
 (* A 3 *)
-isDigit #"7";
+assertsTrue (isDigit #"7") "A" "3";
 (* A 4 *)
 digitValue #"a";
 digitValue #"7";
 (* A 5 *)
 conv 99.99;
 (* A 6 *)
-NAND false true;
+assertsTrue (NAND false true) "A" "6";
 (* B 1 *)
 log2 8;
 (* B 2 *)
@@ -37,5 +45,7 @@ drop 3 [1,2,3,4,5];
 (* C 2 *)
 take 3 ["a","b","c","d","e"];
 (* C 3 *)
-starts [1,2,3] [1,2,3,4];
+assertsTrue (starts [1,2,3] [1,2,3,4]) "C" "3";
+
+
 
