f5eed0378140f2119eaea560667d10c0a03e8d37
Dillon J Fearns <djf1@hw.ac.uk>
2016-10-12 16:32:06 +0100

    Answered C5

--- answers.ml
+++ answers.ml
@@ -66,8 +66,15 @@ fun starts [] l2 = true
   | starts l1 l2 = (((hd l1) = (hd l2)) andalso (starts (tl l1) (tl l2)));
     
 (*4 Write a function to check if list l1 is contained in list l2: *)
-fun contains l1 l2 =
+(* fun contains l1 l2 =
 	if (l2=[]) then false
   else if l1=l2 then true
 	else false;
-    
+*)
+(* 5 Write a function to delete a list from another list *)
+fun delete l1 [] = []
+  | delete l1 l2 = if starts l1 l2 then drop (length l1) l2 else (hd l2)::(delete l1(tl l2)) ;
+
+
+
+
