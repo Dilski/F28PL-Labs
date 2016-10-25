b3b52c06742dd4eac8583afe0acc2d1d9b60b04c
Dilski <dillon.fearns@gmail.com>
2016-10-25 12:54:27 +0100

    Started E

---
+++ answersE.ml
@@ -0,0 +1,4 @@
+E 1 - fun IAnd (x:bool) (y:bool) = x=y;
+2 - 16. There are 2 inputs and each of those inputs can be one of two things. Therefore there are 2^2 combinations of inputs. The output of the function can be one of two, so the possible outputs would be 2^combinations of inputs. Therefore we have 2^2^2 which is 16
+3 - both functions are an identity function, which maps a number to itself (or multiplies by 1)
+
